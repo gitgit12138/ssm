7c214cd3411d33b88ddc1b323407d447e28ea58e
gitgit12138 <3482284853@qq.com>
2020-08-19 15:12:59 +0800

    tenUpdate

--- src/main/java/com/qfedu/service/impl/UserServiceImpl.java
+++ src/main/java/com/qfedu/service/impl/UserServiceImpl.java
@@ -5,12 +5,18 @@ import com.qfedu.pojo.User;
 import com.qfedu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Jayus
  * @create 2020-08-10-2:41 下午
  */
 @Service
+/*
+表示当前类中的所有方法在执行时都会进行事务的开启的提交
+在发生异常时，事务会进行回滚
+ */
+@Transactional
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
